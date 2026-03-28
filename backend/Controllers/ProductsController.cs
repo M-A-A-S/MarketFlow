ea7d299b8a4d9d2fd6fd076cad57b2ef356d88b4
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-03-28 18:05:06 +0200

    Add Products Controller

---
+++ backend/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using MarketFlow.Business.Interfaces;
+using MarketFlow.DTOs.Category;
+using MarketFlow.DTOs.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketFlow.Controllers
+{
+    [Route("api/products")]
+    public class ProductsController : BaseController
+    {
+        private readonly IProductService _service;
+
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] ProductFilterDTO filters)
+        {
+            return FromResult(await _service.GetFilteredAsync(filters));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return FromResult(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] ProductDTO DTO)
+        {
+            return FromResult(await _service.AddAsync(DTO));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromForm] ProductDTO DTO)
+        {
+            return FromResult(await _service.UpdateAsync(id, DTO));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return FromResult(await _service.DeleteAsync(id));
+        }
+    }
+}
