f5a482a25547236a111f57db26035c06675a9bac
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-03-28 12:22:15 +0200

    Add backend folder structure

---
+++ backend/Utilities/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MarketFlow.Utilities
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public int Total { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
