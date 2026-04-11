ba43fff95e74ca134fa00c4ba087efc30d648a4c
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-04-11 13:51:02 +0200

    Add purchases controllers

---
+++ backend/Controllers/PurchaseInvoicesController.cs
@@ -0,0 +1,48 @@
+﻿using MarketFlow.Business.Interfaces;
+using MarketFlow.DTOs.PurchaseInvoice;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketFlow.Controllers
+{
+    [Route("api/purchase-invoices")]
+    public class PurchaseInvoicesController : BaseController
+    {
+        private readonly IPurchaseInvoiceService _service;
+
+        public PurchaseInvoicesController(IPurchaseInvoiceService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return FromResult(await _service.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return FromResult(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PurchaseInvoiceDTO dto)
+        {
+            return FromResult(await _service.AddAsync(dto));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] PurchaseInvoiceDTO dto)
+        {
+            return FromResult(await _service.UpdateAsync(id, dto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return FromResult(await _service.DeleteAsync(id));
+        }
+    }
+}
---
+++ backend/Controllers/PurchasePaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using MarketFlow.Business.Interfaces;
+using MarketFlow.DTOs.PurchasePayment;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketFlow.Controllers
+{
+    [Route("api/purchase-payments")]
+    public class PurchasePaymentsController : BaseController
+    {
+        private readonly IPurchasePaymentService _service;
+
+        public PurchasePaymentsController(IPurchasePaymentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("invoice/{invoiceId}")]
+        public async Task<IActionResult> GetByInvoice(int invoiceId)
+        {
+            return FromResult(await _service.GetByInvoiceIdAsync(invoiceId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] PurchasePaymentDTO dto)
+        {
+            return FromResult(await _service.AddAsync(dto));
+        }
+    }
+}
